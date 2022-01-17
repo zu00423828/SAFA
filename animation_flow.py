3fd54f7f06daa2e9a89b0b173fb01b7243f67628
yuan <zu00423828@gmail.com>
2022-01-17 10:08:45 +0800

    add padd image

--- animation_flow.py
+++ animation_flow.py
@@ -1,4 +1,5 @@
 import pickle
+from tkinter import LEFT
 import cv2
 import numpy as np
 import os
@@ -11,13 +12,11 @@ import face_alignment
 import subprocess
 from tqdm import trange
 
-def concat_video(dir):
-    video1 = cv2.VideoCapture(f'{dir}/source.mp4')
-    video2 = cv2.VideoCapture(f'{dir}/driving.mp4')
-    video3 = cv2.VideoCapture(f'{dir}/result.mp4')
-
+def concat_video(left,right,out_path):
+    video1 = cv2.VideoCapture(left)
+    video2 = cv2.VideoCapture(right)
     out = cv2.VideoWriter(
-        f'{dir}/concat.avi', cv2.VideoWriter_fourcc(*'XVID'), 30.0, (768, 256))
+        out_path, cv2.VideoWriter_fourcc(*'XVID'), 30.0, (512, 256))
     while video1.isOpened():
         ret, frame1 = video1.read()
         if not ret:
@@ -25,10 +24,7 @@ def concat_video(dir):
         ret, frame2 = video2.read()
         if not ret:
             break
-        ret, frame3 = video3.read()
-        if not ret:
-            break
-        frame = np.concatenate([frame1,frame2,frame3], axis=1)
+        frame = np.concatenate([frame1,frame2], axis=1)
         out.write(frame)
     video1.release()
     video2.release()
@@ -64,7 +60,10 @@ def extract_landmark(video_path, out_path):
         if not ret:
             break
         result = fa.get_landmarks(frame)
-        landmark = result[0]
+        if result is None:
+            landmark = None
+        else:
+            landmark = result[0]
         all_landmarks.append(landmark)
 
     with open(out_path, 'wb') as f:
@@ -117,7 +116,27 @@ def create_lb(iters: int = None, ksize: int = 3, sigma=0):
         param.requires_grad = False
     return lb
 
+def quantize_position(x1, x2, y1, y2, iters=None):
+    w = x2 - x1
+    h = y2 - y1
+    x_center = (x2 + x1) // 2
+    y_center = (y2 + y1) // 2
+    half_w = np.math.ceil(w / (2 ** iters)) * \
+        (2 ** (iters - 1))
+    half_h = np.math.ceil(h / (2 ** iters)) * \
+        (2 ** (iters - 1))
+    x1 = x_center - half_w
+    x2 = x_center + half_w
+    y1 = y_center - half_h
+    y2 = y_center + half_h
 
+    if x1 < 0:
+        x2 -= x1
+        x1 = 0
+    if y1 < 0:
+        y2 -= y1
+        y1 = 0
+    return int(x1), int(x2), int(y1), int(y2)
 def paste_origin_video(source_origin_path,safa_video_path,temp_dir,landmark_path,source_video_data_path):
     full_video=cv2.VideoCapture(source_origin_path)
     crop_video=cv2.VideoCapture(safa_video_path)
@@ -126,7 +145,7 @@ def paste_origin_video(source_origin_path,safa_video_path,temp_dir,landmark_path
     w= int(full_video.get(3))
     out_video = cv2.VideoWriter(out_video_path, cv2.VideoWriter_fourcc(
         *'XVID'), 30.0, (w,h))
-    lb=create_lb(3)
+    lb=create_lb(4)
     with open(landmark_path,'rb' ) as f:
         source_landmark=pickle.load(f)
     with open(source_video_data_path,'rb') as f:
@@ -142,11 +161,17 @@ def paste_origin_video(source_origin_path,safa_video_path,temp_dir,landmark_path
         new_frame = cv2.warpAffine(
                 crop_frame, inv_matrix,(w,h), borderMode=cv2.BORDER_CONSTANT, borderValue=(135, 133, 132)) 
         mask=_cal_mouth_contour_mask(source_landmark[i],h,w,None,0.1)
+
+        x1,x2,y1,y2=quantize_position(0,w,0,h,4)
+        if x2>w or y2>h:
+            mask= cv2.copyMakeBorder(mask,0,y2-h,0,x2-w,cv2.BODER_CONTSTANT,value=[255,255,255])
+            full_frame=cv2.copyMakeBorder(full_frame,0,y2-h,0,x2-w,cv2.BODER_CONTSTANT,value=[255,255,255])
+            new_frame=cv2.copyMakeBorder(new_frame,0,y2-h,0,x2-w,cv2.BODER_CONTSTANT,value=[255,255,255])
         mask_tesor=torch.tensor(mask,dtype=torch.float32).permute(2,0,1).unsqueeze(0)
         y_tensor=torch.tensor(full_frame/255,dtype=torch.float32).permute(2,0,1).unsqueeze(0)
         x_tensor=torch.tensor(new_frame/255,dtype=torch.float32).permute(2,0,1).unsqueeze(0)
         out=lb(y_tensor,x_tensor,mask_tesor)
-        full_out=(out[0].permute(1,2,0)*255).numpy().astype(np.uint8)
+        full_out=(out[0][:,:h,:w].permute(1,2,0)*255).numpy().astype(np.uint8)
         out_video.write(full_out)
     out_video.release()
     # out_video2.release()
